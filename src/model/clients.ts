06cd34853f518df15de1f188658888c6d6962879
bruno <ballogggamer@gmail.com>
2023-06-15 02:08:38 -0300

    add saveClient

---
+++ src/model/clients.ts
@@ -0,0 +1,27 @@
+import findPlayer from "./tools/player";
+import { playerProperty } from "../types/playerType";
+
+var Clients: playerProperty[] = [];
+
+/*
+----: get the client and put into clients database
+*/
+
+function saveClient(client: playerProperty): any {
+    if (Clients.length == 0) {
+        console.log("not found any clients into database");
+        Clients.push(client)
+    }
+    else {
+        let isCompare = findPlayer(client.uuid) as playerProperty;
+
+        if (isCompare == undefined) {
+            console.log("yes")
+            Clients.push(client)
+        }
+        else
+            console.log("client existed into database");
+    }
+}
+
+export { saveClient, Clients };
