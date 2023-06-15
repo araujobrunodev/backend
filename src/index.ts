ee2bfd5706a0b69a369c1fefcd141b867517920a
bruno <ballogggamer@gmail.com>
2023-06-15 01:42:01 -0300

    add server

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import http from "http"
+import ws from "ws"
+import { Server as BucketFiles } from "node-static"
+
+import Manager from "./manager";
+import controllers from "./controllers"
+
+const PORT = process.env.PORT ?? 3000
+
+const bucket = new BucketFiles("./public", { indexFile: "index.html" })
+const server = http.createServer((request, response) => {
+  request.on("end", () => {
+    bucket.serve(request, response)
+  }).resume()
+});
+const websocket = new ws.WebSocketServer({ server })
+
+
+controllers(new Manager(websocket))
+
+server.listen(PORT, () => {
+  console.log(`Server up on localhost:${PORT}`)
+})
