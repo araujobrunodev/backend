b598f19a004dac5638465366624c10784f78a4ff
bruno <ballogggamer@gmail.com>
2023-06-15 01:48:38 -0300

    add denied method

---
+++ src/controllers/denied.ts
@@ -0,0 +1,19 @@
+import Manager from "@/manager";
+import findPlayer from "@/model/tools/player";
+import { playerProperty } from "@/types/playerType";
+
+export default (manager:Manager) => {
+    manager.on("DENIED",(context,msg) => {
+        let client1 = findPlayer(context.originId) as playerProperty;
+        let client2 = findPlayer(msg.uuid) as playerProperty;
+
+        if (client1 != undefined && client2 != undefined) {
+            if (!client1.available || !client2.available) return;
+            
+            console.log("denied the invite");
+            
+            context.send("DENIED",client1.name as string ,client2.uuid);
+        } else
+            console.log("don't find client");
+    })
+}
