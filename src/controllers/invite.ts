61da8ad74f3eda96a20f98f0e75a933afa0880f7
bruno <ballogggamer@gmail.com>
2023-06-15 01:50:56 -0300

    add invite method

---
+++ src/controllers/invite.ts
@@ -0,0 +1,17 @@
+import findPlayer from "@/model/tools/player";
+import Manager from "@/manager";
+import { playerProperty } from "@/types/playerType";
+
+export default (manager: Manager) => {
+    manager.on("INVITE", (context, msg) => {
+        let player1 = findPlayer(context.originId) as playerProperty,
+            player2 = findPlayer(msg.uuid) as playerProperty;
+
+        if (player1 != undefined && player2 != undefined) {
+            if (!player1.available || !player2.available) return;
+            console.log("====invite====");
+            context.send("INVITE", { nick: player1.name, uuid: player1.uuid, value: "invite you for a match" }, player2.uuid)
+        } else
+            console.log("can't find player id");
+    })
+}
