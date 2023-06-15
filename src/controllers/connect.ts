49080b53fac9307da3fb7006850820cf581f5c3f
bruno <ballogggamer@gmail.com>
2023-06-15 01:47:19 -0300

    add connet method

---
+++ src/controllers/connect.ts
@@ -0,0 +1,25 @@
+import Manager from "@/manager";
+import { saveClient } from "@/model/clients";
+import PingOrPongReceive from "@/types/pingReceive";
+import tempPlayer from "@/types/tempPlayer";
+
+export default (manager:Manager) => {
+    manager.on("CONNECT",(context,msg) => {
+        context.send("CONNECTED",{uuid: context.originId},context.originId);
+        
+        saveClient({
+            name:msg.player,
+            uuid:context.originId,
+            available:false
+        });
+
+        tempPlayer.uuids.push(context.originId);
+        tempPlayer.uuid = context.originId;
+
+        PingOrPongReceive.push({
+            ping:false,
+            pong:false,
+            uuid:context.originId
+        })
+    })
+}
